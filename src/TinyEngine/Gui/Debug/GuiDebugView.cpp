df59f34790b8eacf786cea21e23d461e804321e2
Андрей Гребенщиков <a-grebenshikov@bk.ru>
2022-03-29 01:28:27 +0500

    Рефакторинг визуала в классе GuiDebugView.

--- src/TinyEngine/Gui/Debug/GuiDebugView.cpp
+++ src/TinyEngine/Gui/Debug/GuiDebugView.cpp
@@ -12,17 +12,24 @@ namespace TinyEngine
 		
 		ImGui::Begin("Debug");
 		ImGui::SetWindowSize({ 500, 500 });
-		ImGui::Text(fmt::format("App name: {}", debugModel.GetAppName()).c_str());
-		ImGui::Text(fmt::format("App version: {}", debugModel.GetAppVersion()).c_str());
+
+		ImGui::Text("App:");
+		ImGui::Text(fmt::format("Name: {}", debugModel.GetAppName()).c_str());
+		ImGui::Text(fmt::format("Version: {}", debugModel.GetAppVersion()).c_str());
 		ImGui::Text(fmt::format("Fps: {}", debugModel.GetFpsCounter()).c_str());
+		ImGui::NewLine();
+
+		ImGui::Text("Render:");
 		ImGui::Text(fmt::format("Layouts count: {}", debugModel.GetLayoutsCount()).c_str());
 		ImGui::Text(fmt::format("Objects count: {}", debugModel.GetObjectsCount()).c_str());
+		ImGui::NewLine();
 
 		ImGui::Text("Actions:");
 		for (const auto& actionId : debugModel.GetActionsIds())
 		{
 			ImGui::Text(actionId.c_str());
 		}
+		ImGui::NewLine();
 
 		ImGui::End();
 	}
