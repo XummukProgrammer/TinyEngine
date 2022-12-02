d610a7a6acac9843484445e942fed12b6a41d257
Andrey Grebenshchikov <a-grebenshikov@bk.ru>
2022-12-03 01:20:48 +0500

    [Raylib] Подключил ImGui

--- cmake/src.cmake
+++ cmake/src.cmake
@@ -1,8 +1,31 @@
 
 set(ENGINE_HEADERS
+	src/extras/IconsFontAwesome6.h
+	src/extras/FA6FreeSolidFontData.h
+
+	src/imgui/imconfig.h
+	src/imgui/imgui.h
+	src/imgui/imgui_internal.h
+	src/imgui/imstb_rectpack.h
+	src/imgui/imstb_textedit.h
+	src/imgui/imstb_truetype.h
+	src/imgui/misc/cpp/imgui_stdlib.h
+
 	src/TinyEngine/TinyEngine.hpp
+	
+	src/TinyEngine/Libs/rlImGui/rlImGui.h
+	src/TinyEngine/Libs/rlImGui/rlImGuiColors.h
 )
 
 set(ENGINE_SOURCES
+	src/imgui/imgui.cpp
+	src/imgui/imgui_demo.cpp
+	src/imgui/imgui_draw.cpp
+	src/imgui/imgui_tables.cpp
+	src/imgui/imgui_widgets.cpp
+	src/imgui/misc/cpp/imgui_stdlib.cpp
+
 	src/TinyEngine/TinyEngine.cpp
+	
+	src/TinyEngine/Libs/rlImGui/rlImGui.cpp
 )
